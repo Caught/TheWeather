336de2d911d7e02c47afeb93f26e93502abe4c30
Caught <caught777@hotmail.com>
2026-09-12 08:58:38 +0200

    Update installer_theweather_auto.sh

--- installer_theweather_auto.sh
+++ installer_theweather_auto.sh
@@ -3,7 +3,7 @@ set -e
 
 REPO="Caught/TheWeather"
 NAME="theweather"
-VERSION="4.3"
+VERSION="4.4"
 
 if command -v dpkg >/dev/null 2>&1; then
     PKG="enigma2-plugin-extensions-${NAME}_${VERSION}_all.deb"
